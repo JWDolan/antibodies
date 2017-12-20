6223ec12ef4bb1543c020c744f102a2555c97efa
James Dolan <jwdolan820@gmail.com>
2017-12-20 04:05:45 +0000

    Implemented secure password to the User model and generated password digest migration to add password_digest column to the User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
    validates :email, presence: true, length: { maximum: 250 },
                            format: { with: VALID_EMAIL_REGEX },
                            uniqueness: case_sensitive: false
+
+   has_secure_password
+   validates :password, presence: true, length: { minimum: 6 }
 end
