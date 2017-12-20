21c6ef2160f2596dfdf562a956b50bf1c554d9f4
James Dolan <jwdolan820@gmail.com>
2017-12-20 20:13:40 +0000

    Added SessionsController and login/logout routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'session/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
 
@@ -11,5 +13,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
